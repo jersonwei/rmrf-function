4522c9ebe7836598d6bbf52ca4730bc20a219302
jesonwei <872937094@qq.com>
2023-02-15 22:33:53 +0800

    ✨ feat(main.ts): simula parse result

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
 
 import {parse} from '@babel/parser'
 import traverse from '@babel/traverse'
-export function getFunctionNode() {
-  
-  const code =  `
-  function getName () {
-      return 'name'
-  }
-  function getNameA () {
-      return 'name'
-  }
-  `
+export function getFunctionNode(code:string,index:number) {
+  let functionNode
   const ast = parse(code)
   traverse(ast,{
     FunctionDeclaration(path){
       console.log(path.node)
+      // functionNode = path.node
+      if(index >= path.node?.start! && index <= path.node?.end!){
+        functionNode = {
+          name:path.node.id?.name,
+          start:path.node?.loc?.start,
+          end:path.node?.loc?.end
+        }
+
+      }
     }
   })
+  return functionNode
 }
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -2,6 +2,24 @@ import { test,expect} from 'vitest'
 import { getFunctionNode } from '../src/main'
 
 test('init',() => {
-  getFunctionNode()
-  expect(true).toBe(true)
+  const code =  `
+  function getName () {
+      return 'name'
+  }
+  function getNameA () {
+      return 'name'
+  }
+  `
+  const index =10
+  const functionCode =  getFunctionNode(code,index)
+
+  expect(functionCode).toEqual({
+    name:'getName',
+    start:{
+      line: 2, column: 2, index: 3
+    },
+    end:{
+      line: 4, column: 3, index: 48
+    }
+  })
 })
